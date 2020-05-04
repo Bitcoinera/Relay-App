b19a16359337824750f9371b3fbbee04ad7dac0b
VG <victor.gil.cif@gmail.com>
2020-05-04 16:59:29 +0200

    Html & CSS clean

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -3,23 +3,23 @@ function uploadFunction(){
   var txt = "";
   if ('files' in x) {
     if (x.files.length == 0) {
-      txt = "Select one or more files.";
+      txt = "Select one.";
     } else {
       for (var i = 0; i < x.files.length; i++) {
-        txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+        txt += "<br><strong>" + (i+1) + " Archivo</strong><br>";
         var file = x.files[i];
         if ('name' in file) {
-          txt += "name: " + file.name + "<br>";
+          txt += "Nombre: " + file.name + "<br>";
         }
         if ('size' in file) {
-          txt += "size: " + file.size + " bytes <br>";
+          txt += "Tamaño: " + file.size + " bytes <br>";
         }
       }
     }
   } 
   else {
     if (x.value == "") {
-      txt += "Select one or more files.";
+      txt += "Select one.";
     } else {
       txt += "The files property is not supported by your browser!";
       txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
